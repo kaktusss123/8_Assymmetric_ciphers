4ac7ddbe5f68498609b067da628615e5278b2517
Влад <komarnitskij.v@phystech.edu>
2019-12-21 14:14:08 +0300

    обмен ключами работает

--- client.py
+++ client.py
@@ -1,14 +1,33 @@
-import socket
-import pickle
+from socket import socket
+from random import randint
+from pickle import dumps, loads
 
-HOST = '127.0.0.1'
-PORT = 8080
+def send(sock, msg):
+    msg = msg.encode()
+    header = f'{len(msg):10}'.encode()
+    sock.send(header + msg)
 
-sock = socket.socket()
-sock.connect((HOST, PORT))
+def recv(sock):
+    header = sock.recv(10)
+    header = int(header.decode())
+    msg = sock.recv(header)
+    return msg.decode()
 
-p, g, a = 7, 5, 3
-A = g ** a % p
-sock.send(pickle.dumps((p, g, A)))
 
-sock.close()
+def auth(sock):
+    a, g, p = (randint(100, 100000) for _ in range(3))
+    A = g ** a % p
+    sock.send(dumps((A, g, p)))
+    B = loads(sock.recv(1024))
+    K = B ** a % p
+    return K
+
+if __name__ == "__main__":
+    with open('port') as f:
+        port = int(f.read())
+    
+    sock = socket()
+    sock.connect(('localhost', port))
+    key = auth(sock)
+    print(f'Client key: {key}')
+
--- server.py
+++ server.py
@@ -1,15 +1,43 @@
-import socket
-import pickle
+from socket import socket
+from pickle import dumps, loads
+from random import randint
 
-HOST = '127.0.0.1'
-PORT = 8080
+def send(sock, msg):
+    msg = msg.encode()
+    header = f'{len(msg):10}'.encode()
+    sock.send(header + msg)
 
-sock = socket.socket()
-sock.bind((HOST, PORT))
-sock.listen(1)
-conn, addr = sock.accept()
+def recv(sock):
+    header = sock.recv(10)
+    header = int(header.decode())
+    msg = sock.recv(header)
+    return msg.decode()
 
-msg = conn.recv(1024)
-print(pickle.loads(msg))
+def auth(sock):
+    A, g, p = loads(sock.recv(1024))
+    b = randint(100, 100000)
+    B = g ** b % p
+    sock.send(dumps(B))
+    K = A ** b % p
+    return K
 
-conn.close()
+
+if __name__ == '__main__':
+    sock = socket()
+    try:
+        sock.bind(('', 8080))
+    except OSError:
+        sock.bind(('', 0))
+    finally:
+        with open('port', 'w') as f:
+            f.write(str(sock.getsockname()[1]))
+
+    sock.listen(1)
+    try:
+        while 1:
+            conn, addr = sock.accept()
+            key = auth(conn)
+            print(f'Server key: {key}')
+            conn.close()
+    finally:
+        sock.close()
